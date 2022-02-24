2415b156226c4591ca7d5d300c32ac48434912fa
ChinJiaWei <jiawei202047@dasein.edu.my>
2022-02-24 16:02:59 +0800

    add date

--- array.php
+++ array.php
@@ -8,31 +8,27 @@
         width: 15px;
         height: 15px;
         padding: 10px;
+        margin:15px;
         border: 2px solid #000;
         text-align: center;
         display: inline-block;
+        
       }
-      class.arrange {
-         display: inline-block;             
-      }
+
     </style>
 
   <!--original element in sequence-->
   
- <class="arrange">
+
 <?php
-$num = array(34,4,5,-23,100);
-echo "<class>$num[0] </div>" . "<class>$num[1] </div>" . "<class>$num[2] </div>" . "<class>$num[3] </div>" . "<class>$num[4] </div>";
+$num = array("<class>34</div>","<class>4</div>","<class>5</div>","<class>-23</div>","<class>100</div>");
+echo $num[0]. $num[1]. $num[2]. $num[3]. $num[4];
 ?>
-</div>
 
-<br>
   <!--swap first and last element-->
-  <?php
-$num = array(34, 4, 5, -23, 100);
-echo $num[4] . ", " . $num[1] . " , " . $num[2] . "," . $num[3] . "," . $num[0];
+<?php
+echo $num[4]. $num[1]. $num[2]. $num[3]. $num[0];
 ?>
-
 
 </body>  
-</html>  
+</html>   
