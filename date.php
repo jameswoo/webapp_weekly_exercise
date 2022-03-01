42dae3bf2c07db2e4dfdcdb73db524d4e0559f2f
ChinJiaWei <jiawei202047@dasein.edu.my>
2022-03-01 09:17:44 +0800

    make date inline

--- date.php
+++ date.php
@@ -1,29 +1,50 @@
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" href="style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <title>Q2 date</title>
+
 <body>
-    <!--1st calendar-->
-    <div class="col-sm-12 col-xl-4">
+    <div class="d-lg-flex flex-row d-flex flex-row my-5">
+        <!--1st calendar-->
+        <div id="date1 flex-sm-row" class="container col-7 col-lg-3 text-center">
+            <div class=" container.col-12 col-xl-4 h1">
+                <?php
+                echo date("d");
+                ?>
+                <div class="border-top border-dark text-center">
+                    <sup>
+                        <div class="h6 text-uppercase d-inline">
+                            <?php
+                            echo date("M Y");
+                            ?>
+                        </div>
+                    </sup>
+                </div>
+
+            </div>
 
 
-
-    </div>
-
-    <?php
-    $date = date("<text>d</text>/M/Y");
-    echo $date;
-    ?>
+        </div>
 
 
-    <!--2st calendar-->
+        <!--2st calendar-->
+        <div id="date1" class="container col-7 col-lg-3 text-center">
+            <div class=" container.col-12 col-xl-4 h1 border ">
+                <?php
+                echo date("d");
+                ?>
+                <br>
+                <div class="h6 text-uppercase border p-1 px-3 d-inline ">
+                    <?php
+                    echo date("M Y");
+                    ?>
+                </div>
+            </div>
 
-    <?php
-    $date = date("d/M/Y");
-    echo $date;
-    ?>
-<!--3rd date type-->
-<div id="date3" class="container-lg col-8 col-lg-2 text-center">
+        </div>
+
+        <!--3rd date type-->
+        <div id="date3" class="container-lg col-7 col-lg-3 text-center">
             <div class="h1 text-uppercase text-center py-3 d-inline">
                 <?php
                 echo "<strong>";
@@ -40,7 +61,7 @@
             </div>
 
         </div>
-    ?>
+        </div>
 </body>
 
 </html>
