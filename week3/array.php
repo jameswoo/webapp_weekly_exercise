b0cd0756c752639b26f46c231b69d29d5b9be911
ChinJiaWei <jiawei202047@dasein.edu.my>
2022-03-03 23:48:18 +0800

    array done

--- week3/array.php
+++ week3/array.php
@@ -5,33 +5,65 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
     <?php
     $numbers = array("34", "4", "5", "-23", "45", "100");
-    $quantity = count($numbers);
-    for ($x = 0; $x < $quantity; $x++) {
-
-        echo "<div class='col-lg-1 text-center border border-radius my-4 rounded-circle border-dark p-4 p-lg-5'>";
-        echo $numbers[$x];
-        echo "</div>";
-    }
     ?>
+    <div class="container mt-5">
+        <div class="row text-center">
+            <?php
+            for ($i = 0; $i <= count($numbers) - 1; $i++) {
+            ?>
+                <div class="col">
+                    <div class="row justify-content-center">
+                        <div class="col-4 numberCircle">
+                            <p class="fs-5">
+                                <?php
+                                echo $numbers[$i];
+                                ?>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
+        </div>
+    </div>
 
-  </br>
     <?php
-    $numbers[0] = "100";
-    $numbers[5] = "34";
-
-    for ($x = 0; $x < $quantity; $x++) {
-
-        echo "<div class='col-lg-1 text-center border border-radius my-4 rounded-circle border-dark p-4 p-lg-5'>";
-        echo $numbers[$x];
-        echo "</div>";
-    }
+    $numbers = array("34", "4", "5", "-23", "45", "100");
+    $temp = $numbers[0];
+    $numbers[0] = $numbers[4];
+    $numbers[4] = $temp;
     ?>
+
+    <div class="container mt-5">
+    <div class="row text-center">
+        <?php
+        for ($i = 0; $i <= count($numbers) - 1; $i++) {
+        ?>
+            <div class="col">
+                <div class="row justify-content-center">
+                    <div class="col-4 numberCircle">
+                        <p class="fs-5">
+                            <?php
+                            echo $numbers[$i];
+                            ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        <?php
+        }
+        ?>
+    </div>
+</div>
 
 </body>
+
 </html>
