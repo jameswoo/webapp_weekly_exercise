b1b824d52bc6c25c7bd05a807b87e0915390e2f4
ChinJiaWei <jiawei202047@dasein.edu.my>
2022-03-04 00:33:30 +0800

    calendar 1 n 2 done

--- week3/date.php
+++ week3/date.php
@@ -5,75 +5,84 @@
 <title>Q2 date</title>
 
 <body>
-    <!-- <div class="d-lg-flex flex-row d-flex flex-row my-5">-->
-    <!--1st calendar-->
-    <div class="row">
-        <div class="col-7 col-xl-3">
-            <div id="date1 flex-sm-row" class="container text-center">
-                <div class=" container col-12 col-xl-4 h1">
-                    <?php
-                    echo date("d");
-                    ?>
-                    <div class="border-top border-dark text-center">
-                        <sup>
+
+    <div class="container mt-5">
+        <div class="row text-center">
+            <div class="col">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-8 col-lg-4">
+                        <div class="display-2">
+                            <?php
+                            echo "<strong>";
+                            echo date("d");
+                            echo "</strong>";
+                            ?>
+                        </div>
+                        <div class="border-top border-dark text-center pb-1">
                             <div class="h6 text-uppercase d-inline">
                                 <?php
                                 echo date("M Y");
                                 ?>
                             </div>
-                        </sup>
+                        </div>
                     </div>
-
                 </div>
+
             </div>
 
-        </div>
 
 
-        <!--2st calendar-->
-        <div id="date2" class="container col-7 col-xl-3 d-flex align-items-start flex-column ">
-            <div class="h1 border border-dark text-center pt-2 mb-0">
-                <?php
-                echo date("d");
-                ?>
-
-                <div class="h6 text-uppercase border d-flex bg-dark text-white p-1 mb-0">
-                    <?php
-                    echo date("M Y");
-                    ?>
-                </div>
-            </div>
-
-        </div>
-
-        <!--3rd calendar-->
-        <div id="date3" class="container col-7 col-xl-3 text-center d-flex align-items-start">
-            <div class="h1 text-uppercase text-center py-3 d-cen">
-                <?php
-                echo "<strong>";
-                echo date("d");
-                echo "</strong>";
-                ?>
-                <sup>
-                    <div class="h6 text-uppercase d-inline ">
+            <!--2st calendar-->
+            <div class="col ">
+                <div class="col-12 col-md-8 col-lg-4">
+                    <div class="border border-dark border-2 border-bottom-0">
+                            <div class="display-2">
+                                <?php
+                                echo "<strong>";
+                                echo date("d");
+                                echo "</strong>";
+                                ?>
+                        </div>
+                    </div>
+                    <div class="h6 text-uppercase border bg-dark text-white p-1">
                         <?php
-                        echo date("M");
+                        echo date("M Y");
                         ?>
                     </div>
+                </div>
+            </div>
 
-                    <div class="h6 ps-5">
-                        <p class="text-break">
-
-                            <?php
-                            echo date("Y");
-                            ?>
-
-                        </p>
+            <!--3rd calendar-->
+            <div class="col">
+                <div class="col-12 col-md-8 col-lg-4">
+                    <div class="display-2">
+                        <?php
+                        echo "<strong>";
+                        echo date("d");
+                        echo "</strong>";
+                        ?>
                     </div>
-                </sup>
+                    <sup>
+                        <div class="h6 text-uppercase d-inline ">
+                            <?php
+                            echo date("M");
+                            ?>
+                        </div>
+
+                        <div class="h6 ps-5">
+                            <p class="text-break">
+
+                                <?php
+                                echo date("Y");
+                                ?>
+
+                            </p>
+                        </div>
+                    </sup>
+                </div>
             </div>
         </div>
-
+    </div>
 
     </div>
 </body>
