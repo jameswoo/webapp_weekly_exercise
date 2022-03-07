5d8fa3ba13fccb6f5ba6789971f81ab099388004
ChinJiaWei <jiawei202047@dasein.edu.my>
2022-03-07 11:28:39 +0800

    formcalculator done

--- week4/calculateArea.php
+++ week4/calculateArea.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<head><title>findCirclePerimeter</title></head>
+<head><title>findCircleArea</title></head>
 
 <body>
     
---
+++ week4/formCalculate.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head><title>findCirclePerimeter</title></head>
+
+<body>
+
+    <?php
+    $radius = $_POST['radius'];
+    $pi = 3.14;
+    $area1 = $pi * $radius * $radius;
+    $area2 =2*$pi*$radius;
+    echo "Perimeter of circle = $area1";
+?>
+
+
+
+</body>
+
+</html>
