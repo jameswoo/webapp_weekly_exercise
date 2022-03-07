c3f0de1f6fa399f1c9ce862b61e37978a330a8f4
ChinJiaWei <jiawei202047@dasein.edu.my>
2022-03-07 10:04:50 +0800

    area n perimeter of circle

---
+++ week4/calculateArea.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head><title>findCirclePerimeter</title></head>
+
+<body>
+    
+
+    <?php
+    $radius=2;
+    $pi = 3.14;
+    $area1= $pi * $radius * $radius;
+    $area2=2*$pi*$radius;
+    echo "Area of circle = $area1";
+?>
+</body>
+
+</html>
---
+++ week4/calculatePerimeter.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head><title>findCirclePerimeter</title></head>
+
+<body>
+    
+
+    <?php
+    $radius=2;
+    $pi = 3.14;
+    $area1= $pi * $radius * $radius;
+    $area2=2*$pi*$radius;
+    echo "Perimeter of circle = $area2";
+?>
+</body>
+
+</html>
