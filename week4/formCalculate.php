a95d61c1556cf3247557a75d2680665308c93e6e
ChinJiaWei <jiawei202047@dasein.edu.my>
2022-03-08 11:16:30 +0800

    delete html

--- week4/formCalculate.php
+++ week4/formCalculate.php
@@ -7,17 +7,28 @@
 
 <body>
 
+<form action="formCalculate.php" method="post">
+        Radius: <input type="text" name="radius"><br>
+        <input type="submit">
+    </form>
+    
     <?php
-    $radius = $_POST['radius'];
-    $pi = 3.14;
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $radius = $_POST['radius'];
+        $pi = 3.14;
 
+        echo "Area of circle =";
+        echo findCircleArea($radius, $pi);
+    }else {
+        echo "please input radius";
+    }
     function findCircleArea($radius, $pi)
     {
         echo $pi * $radius * $radius;
     }
 
-    echo "Area of circle =";
-    echo findCircleArea("$radius", "$pi");
+
+
     ?>
 
 
--- week4/formValidateresult.php
+++ week4/formValidateresult.php
@@ -1,3 +1,15 @@
+<?php
+function findCircleArea($radius)
+{
+    echo 3.14 * (int)$radius * (int)$radius;
+}
+
+function findCirclePerimeter($radius)
+{
+    echo 2 * 3.14 * $radius;
+}
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -6,30 +18,38 @@
 </head>
 
 <body>
+    <form action="formValidateresult.php" method="post">
+        Radius: <input type="text" name="radius"><br>
+        
+        <div class="form-group row">
+            <div class="offset-sm-2 col-sm-10">
+                <input type="submit" name="submit" value="Submit" class="btn btn-info">
+            </div>
+        </div>
+    </form>
 
     <?php
-    $radius = $_POST['radius'];
-    $pi = 3.14;
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    function findCircleArea ($radius, $pi){
-        echo $pi * $radius * $radius;
+        $radius = $_POST['radius'];
+        $pi = 3.14;
+        if (empty($radius)) {
+            echo "please input radius";
+        } elseif (!is_numeric($radius)) {
+            echo "please only enter number";
+        } else {
+
+            echo "Area of circle =";
+            echo findCircleArea($radius);
+            echo "<br>";
+            echo "Perimeter of circle =";
+            echo findCirclePerimeter($radius);
+        }
     }
-    
-    echo "Area of circle =";
-    echo findCircleArea("$radius", "$pi");
-    echo "<br>";
+
+
     ?>
 
-<?php
-    $radius = $_POST['radius'];
-    $pi = 3.14;
-
-    function findCirclePerimeter ($radius, $pi){
-        echo 2 * $pi * $radius;
-    }
-    
-    echo "Perimeter of circle =";
-    echo findCirclePerimeter("$radius", "$pi");
-
-    ?>
 </body>
+
+</html>
