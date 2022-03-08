248242836810626a8f84a4780ec02d174f7ee62c
ChinJiaWei <jiawei202047@dasein.edu.my>
2022-03-08 11:16:42 +0800

    change form calculation

--- week4/calculateArea.php
+++ week4/calculateArea.php
@@ -2,24 +2,31 @@
 <html>
 
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>findCircleArea</title>
 </head>
 
 <body>
 
+    <div class="p-3 m-5 bg-info text-white text-center ">
 
-    <?php
-    $radius = 2;
-    $pi = 3.14;
+        <?php
+        $radius = 2;
 
-    function findCircleArea ($radius, $pi){
-        echo $pi * $radius * $radius;
-    }
-    
-    echo "Area of circle =";
-    echo findCircleArea("$radius", "$pi");
+        function findCircleArea($radius)
+        {
+            echo 3.14 * $radius * $radius;
+        }
+        echo "Area of circle =";
+        echo findCircleArea($radius);
 
-    ?>
+        ?>
+    </div>
+
 </body>
 
 </html>
--- week4/calculatePerimeter.php
+++ week4/calculatePerimeter.php
@@ -2,24 +2,30 @@
 <html>
 
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>findCirclePerimeter</title>
 </head>
 
 <body>
 
+    <div class="p-3 m-5 bg-info text-white text-center ">
+        <?php
+        $radius = 2;
 
-    <?php
-    $radius = 2;
-    $pi = 3.14;
+        function findCirclePerimeter($radius)
+        {
+            echo 2 * 3.14 * $radius;
+        }
 
-    function findCirclePerimeter ($radius, $pi){
-        echo 2 * $pi * $radius;
-    }
-    
-    echo "Perimeter of circle =";
-    echo findCirclePerimeter("$radius", "$pi");
+        echo "Perimeter of circle =";
+        echo findCirclePerimeter($radius);
 
-    ?>
+        ?>
+    </div>
 </body>
 
 </html>
