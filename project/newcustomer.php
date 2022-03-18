598fbe637c2332a69854f005bdff2728a2b3763e
ChinJiaWei <jiawei202047@dasein.edu.my>
2022-03-18 11:41:58 +0800

    newcustomerdone

--- project/newcustomer.php
+++ project/newcustomer.php
@@ -15,6 +15,7 @@
         </div>
 
         <?php
+
         if ($_POST) {
 
             // include database connection
@@ -28,75 +29,71 @@
             $password = $_POST['password'];
             $confirmpassword = $_POST['confirmpassword'];
             $dob = $_POST['year'] . "-" . $_POST['month'] . "-" . $_POST['day'];
+            $age = date('Y') - $_POST['year'];
+            $error = "";
 
 
-            if (!empty($username) && !empty($firstname) && !empty($lastname) && !empty($gender) && !empty($email) && !empty($password) && !empty($confirmpassword)) {
-
-                //A-Z,a-z,more than 6
-                if (preg_match('/^((?:\s*[A-Za-z]\s*){6,})$/', $username)) {
-
-
-                    //0-9, A-Z,a-z,more than 6 word
-                    if (preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{6,}$/', $password)) {
-
-                        if ($_POST["password"] == $_POST["confirm_password"]) {
-
-                            if (date('Y') - $_POST['year'] >= 18) {
-
-                                try {
-                                    // insert query
-                                    $query = "INSERT INTO customer SET 
-                                    username =:username, 
-                                    firstname =:firstname, 
-                                    lastname =:lastname, 
-                                    dob =:dob,
-                                    gender =:gender,
-                                    email =:email,
-                                    password = :password";
-
-                                    // prepare query for execution
-                                    $stmt = $con->prepare($query);
-
-                                    // bind the parameters
-                                    $stmt->bindParam(':username', $username);
-                                    $stmt->bindParam(':firstname', $firstname);
-                                    $stmt->bindParam(':lastname', $lastname);
-                                    $stmt->bindParam(':dob', $dob);
-                                    $stmt->bindParam(':gender', $gender);
-                                    $stmt->bindParam(':email', $email);
-                                    $stmt->bindParam(':password', $password);
-
-
-                                    // specify when this record was inserted to the database
-                                    //date_default_timezone_set("Asia/Kuala_Lumpur");
-                                    //$dob = date('Y-m-d');
-                                    //
-                                    // Execute the query
-                                    if ($stmt->execute()) {
-                                        echo "<div class='alert alert-success'>Record was saved.</div>";
-                                    } else {
-                                        echo "<div class='alert alert-danger'>Unable to save record.</div>";
-                                    }
-                                }
-                                // show error
-                                catch (PDOException $exception) {
-                                    die('ERROR: ' . $exception->getMessage());
-                                }
-                            } else {
-                                echo "<div class='alert alert-danger'>Customer must be above 18 years old</div>";
-                            }
-                        } else {
-                            echo "<div class='alert alert-danger'>Password and confirm password does not match.</div>";
-                        }
-                    } else {
-                        echo "<div class='alert alert-danger'>Password must be minimum 6 characters, contain at least a number, a capital letter and a small letter</div>";
-                    }
-                } else {
-                    echo "<div class='alert alert-danger'>Username must not contain space with minimum 6 characters</div>";
-                }
+            if (empty($username) || empty($firstname) || empty($lastname) || empty($gender) || empty($email) || empty($password) || empty($confirmpassword)) {
+                $error = $error . "<div class='alert alert-danger'>Please fill in all the information</div>";
             } else {
-                echo "<div class='alert alert-danger'>Please fill in all the information</div>";
+                if (!preg_match('/^((?:\s*[A-Za-z]\s*){6,})$/', $username)) {
+                    $error = $error . "<div class='alert alert-danger'>Username must not contain space with minimum 6 characters</div>";
+                }
+
+                if (!preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z]{6,}$/', $password)) {
+                    $error = $error . "<div class='alert alert-danger'>Password must be minimum 6 characters, contain at least a number, a capital letter and a small letter</div>";
+                } else if ($password  != $confirmpassword) {
+                    $error = $error . "<div class='alert alert-danger'>Password and confirm password does not match.</div>";
+                }
+
+                if ($age <= 18) {
+                    $error = $error . "<div class='alert alert-danger'>Customer must be above 18 years old</div>";
+                }
             }
+
+            if ($error = "") {
+                try {
+                    // insert query
+                    $query = "INSERT INTO customer SET 
+                    username =:username, 
+                    firstname =:firstname, 
+                    lastname =:lastname, 
+                    dob =:dob,
+                    gender =:gender,
+                    email =:email,
+                    password = :password";
+
+                    // prepare query for execution
+                    $stmt = $con->prepare($query);
+
+                    // bind the parameters
+                    $stmt->bindParam(':username', $username);
+                    $stmt->bindParam(':password', $password);
+                    $stmt->bindParam(':confirmpassword', $confirmpassword);
+                    $stmt->bindParam(':email', $email);
+                    $stmt->bindParam(':firstname', $firstname);
+                    $stmt->bindParam(':lastname', $lastname);
+                    $stmt->bindParam(':dob', $dob);
+                    $stmt->bindParam(':gender', $gender);
+
+
+                    // specify when this record was inserted to the database
+                    //date_default_timezone_set("Asia/Kuala_Lumpur");
+                    //$dob = date('Y-m-d');
+                    // Execute the query
+                    if ($stmt->execute()) {
+                        echo "<div class='alert alert-success'>Record was saved.</div>";
+                    } else {
+                        echo "<div class='alert alert-danger'>Unable to save record.</div>";
+                    }
+                }
+                // show error
+                catch (PDOException $exception) {
+                    die('ERROR: ' . $exception->getMessage());
+                }
+            }
+
+            echo $error;
         }
 
         ?>
@@ -106,17 +103,17 @@
                 <tr>
                     <td>Username</td>
                     <td>
-                        <input type='text' name='username' class='form-control' pattern='^((?:\s*[A-Za-z]\s*){6,})$' />
+                        <input type='text' name='username' class='form-control' />
                     </td>
                 </tr>
                 <tr>
                     <td>Password</td>
-                    <td><input type='text' name='password' class='form-control' pattern="(?=.*\d)(?=.*][a-z)(?=.*[A-Z]).{6,}" />
+                    <td><input type='password' name='password' class='form-control' />
                     </td>
                 </tr>
                 <tr>
                     <td>Confirm Password</td>
-                    <td><input type='text' name='confirmpassword' class='form-control' pattern="$_POST['password']" /></td>
+                    <td><input type='password' name='confirmpassword' class='form-control' /></td>
                 </tr>
                 <tr>
                     <td>Email</td>
@@ -131,14 +128,14 @@
                     <td><input type='text' name='lastname' class='form-control' /></td>
                 </tr>
                 <tr>
-                    
+
                     <div class="<d-flex flex-row mb-3">
                         <td>Date of Birth</td>
-                        <td>
-                            <div class="w-25 p-2">
+                        <td class="btn-group w-100">
+                            <div class="w-30 p-2">
                                 <label for="day">Day:</label>
                                 <select name='day' class="form-control" id="day">
-                                <option selected>--- Choose day ---</option>
+                                    <option selected>--- Choose day ---</option>
 
                                     <?php
                                     for ($day = 1; $day <= 31; $day++) {
@@ -152,15 +149,15 @@
                                 </select>
                             </div>
 
-                            <div class="w-25 p-2">
+                            <div class="w-40 p-2">
                                 <label for="month ">Month: </label>
                                 <select name='month' class="form-control" id="month">
-                                <option selected>--- Choose month ---</option>
+                                    <option selected>--- Choose month ---</option>
                                     <?php
                                     $month = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 
                                     for ($x = 0; $x < 12; $x++) {
-                                        
+
                                         echo "<option>";
                                         echo $month[$x];
                                         echo "</option>";
@@ -170,14 +167,14 @@
                                 </select>
                             </div>
 
-                            <div class="w-25 p-2">
+                            <div class="w-30 p-2">
                                 <label for="year">Year:</label>
                                 <select name='year' class="form-control" id="year">
-                                <option selected>--- Choose year ---</option>
+                                    <option selected>--- Choose year ---</option>
                                     <?php
 
                                     for ($year = 1990; $year <= date("Y"); $year++) {
-                                        echo "<option>";
+                                        echo "<option value='$year'>";
                                         echo $year;
                                         echo "</option>";
                                     }
@@ -193,23 +190,23 @@
                 <tr>
                     <td>Gender</td>
                     <td>
-
+                        <!-- must use id -->
                         <div class="pe-1">
-                            <input class="form-check-input" type="radio" value="male" name="gender">
+                            <input class="form-check-input" type="radio" id="male" name="gender">
                             <label class="from-check-label" for="Radio1">
                                 Male
                             </label>
                         </div>
 
                         <div class="pe-1">
-                            <input class="form-check-input" type="radio" value="female" name="gender">
+                            <input class="form-check-input" type="radio" id="female" name="gender">
                             <label class="from-check-label" for="Radio2">
                                 Female
                             </label>
                         </div>
 
                         <div class="pe-1">
-                            <input class="form-check-input" type="radio" value="other" name="gender">
+                            <input class="form-check-input" type="radio" id="other" name="gender">
                             <label class="from-check-label" for="Radio3">
                                 Other
                             </label>
